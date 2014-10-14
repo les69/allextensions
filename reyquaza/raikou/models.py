b965a43975380a88bbddb55b30a21a9688484321
les <nanotrippolo@gmail.com>
2014-10-14 11:02:38 +0200

    project structure

--- reyquaza/raikou/models.py
+++ reyquaza/raikou/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Program(models.Model):
+    name = models.CharField()
+    website = models.URLField(null="true")
+    picture = models.ImageField(upload_to="images/program_pictures/", default="images/defaults/default_program.jpg")
+    description = models.CharField(max_length=250, default="")
+
+class Extension(models.Model):
+    type = models.CharField(max_length=250)
+    description = models.CharField(max_length=300, default="")
+    program = models.ManyToManyField(Program, related_name="programs")
+    icon = models.ImageField(upload_to="images/icon_pictures/", default="images/defaults/default_icon.jpg")
+
+    def _do_stuff(self):
+        print 'make me do something'
+
+
+
+
+@receiver(post_save, sender=Extension)
+def post_save_do_stuff(sender, **kwargs):
+    """ Calls the do_stuff function on the saved instance if it was
+        just created
+    """
+    if kwargs['created']:
+        kwargs['instance']._do_stuff()
--- reyquaza/reyquaza/urls.py
+++ reyquaza/reyquaza/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+
 )
+
+urlpatterns += patterns('suicune.views',
+                        url(r'^$', 'index', name='index')
+                        )
